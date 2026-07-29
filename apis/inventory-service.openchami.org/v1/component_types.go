de1d70604bf180940d286ebb3feabf93d6769494
Shane Unruh <shane.unruh@hpe.com>
2026-07-28 20:35:21 -0600

    Added struct tags for yaml
    Added yaml struct tags that mirror the json tags
    
    Signed-off-by: Shane Unruh <shane.unruh@hpe.com>

--- apis/inventory-service.openchami.org/v1/component_types.go
+++ apis/inventory-service.openchami.org/v1/component_types.go
@@ -21,23 +21,23 @@ type Component struct {
 
 type ComponentSpec struct {
 	Description string `json:"description,omitempty" yaml:"description,omitempty" validate:"max=200"`
-	ID          string `json:"ID"`
+	ID          string `json:"ID" yaml:"ID"`
 
-	Type     string      `json:"Type"`
-	State    string      `json:"State,omitempty"`
-	Flag     string      `json:"Flag,omitempty"`
-	Enabled  *bool       `json:"Enabled,omitempty"`
-	SwStatus string      `json:"SoftwareStatus,omitempty"`
-	Role     string      `json:"Role,omitempty"`
-	SubRole  string      `json:"SubRole,omitempty"`
-	NID      json.Number `json:"NID,omitempty"`
-	Subtype  string      `json:"Subtype,omitempty"`
-	NetType  string      `json:"NetType,omitempty"`
-	Arch     string      `json:"Arch,omitempty"`
-	Class    string      `json:"Class,omitempty"`
+	Type     string      `json:"Type" yaml:"Type"`
+	State    string      `json:"State,omitempty" yaml:"State,omitempty"`
+	Flag     string      `json:"Flag,omitempty" yaml:"Flag,omitempty"`
+	Enabled  *bool       `json:"Enabled,omitempty" yaml:"Enabled,omitempty"`
+	SwStatus string      `json:"SoftwareStatus,omitempty" yaml:"SoftwareStatus,omitempty"`
+	Role     string      `json:"Role,omitempty" yaml:"Role,omitempty"`
+	SubRole  string      `json:"SubRole,omitempty" yaml:"SubRole,omitempty"`
+	NID      json.Number `json:"NID,omitempty" yaml:"NID,omitempty"`
+	Subtype  string      `json:"Subtype,omitempty" yaml:"Subtype,omitempty"`
+	NetType  string      `json:"NetType,omitempty" yaml:"NetType,omitempty"`
+	Arch     string      `json:"Arch,omitempty" yaml:"Arch,omitempty"`
+	Class    string      `json:"Class,omitempty" yaml:"Class,omitempty"`
 
-	ReservationDisabled bool `json:"ReservationDisabled,omitempty"`
-	Locked              bool `json:"Locked,omitempty"`
+	ReservationDisabled bool `json:"ReservationDisabled,omitempty" yaml:"ReservationDisabled,omitempty"`
+	Locked              bool `json:"Locked,omitempty" yaml:"Locked,omitempty"`
 }
 
 type ComponentStatus struct {
--- apis/inventory-service.openchami.org/v1/componentendpoint_types.go
+++ apis/inventory-service.openchami.org/v1/componentendpoint_types.go
@@ -14,37 +14,37 @@ type ComponentEndpoint struct {
 	APIVersion string                  `json:"apiVersion" yaml:"apiVersion"`
 	Kind       string                  `json:"kind" yaml:"kind"`
 	Metadata   fabrica.Metadata        `json:"metadata" yaml:"metadata"`
-	ID         string                  `json:"id,omitempty"`
+	ID         string                  `json:"id,omitempty" yaml:"id,omitempty"`
 	Spec       ComponentEndpointSpec   `json:"spec" yaml:"spec" validate:"required"`
 	Status     ComponentEndpointStatus `json:"status,omitempty" yaml:"status,omitempty"`
 }
 
 type ComponentEndpointSpec struct {
 	Description string `json:"description,omitempty" yaml:"description,omitempty" validate:"max=200"`
-	ID          string `json:"ID"`
+	ID          string `json:"ID" yaml:"ID"`
 
-	Type   string `json:"Type"`
-	Domain string `json:"Domain,omitempty"`
+	Type   string `json:"Type" yaml:"Type"`
+	Domain string `json:"Domain,omitempty" yaml:"Domain,omitempty"`
 
-	FQDN                string `json:"FQDN,omitempty"`
-	RedfishType         string `json:"RedfishType"`
-	RedfishSubtype      string `json:"RedfishSubtype"`
-	MACAddr             string `json:"MACAddr,omitempty"`
-	UUID                string `json:"UUID,omitempty"`
-	OdataID             string `json:"OdataID"`
-	RfEndpointID        string `json:"RedfishEndpointID"`
-	Enabled             bool   `json:"Enabled"`
-	RedfishEndpointFQDN string `json:"RedfishEndpointFQDN,omitempty"`
-	URL                 string `json:"RedfishURL,omitempty"`
+	FQDN                string `json:"FQDN,omitempty" yaml:"FQDN,omitempty"`
+	RedfishType         string `json:"RedfishType" yaml:"RedfishType"`
+	RedfishSubtype      string `json:"RedfishSubtype" yaml:"RedfishSubtype"`
+	MACAddr             string `json:"MACAddr,omitempty" yaml:"MACAddr,omitempty"`
+	UUID                string `json:"UUID,omitempty" yaml:"UUID,omitempty"`
+	OdataID             string `json:"OdataID" yaml:"OdataID"`
+	RfEndpointID        string `json:"RedfishEndpointID" yaml:"RedfishEndpointID"`
+	Enabled             bool   `json:"Enabled" yaml:"Enabled"`
+	RedfishEndpointFQDN string `json:"RedfishEndpointFQDN,omitempty" yaml:"RedfishEndpointFQDN,omitempty"`
+	URL                 string `json:"RedfishURL,omitempty" yaml:"RedfishURL,omitempty"`
 
-	ComponentEndpointType string `json:"ComponentEndpointType"`
+	ComponentEndpointType string `json:"ComponentEndpointType" yaml:"ComponentEndpointType"`
 
-	RedfishChassisInfo *ComponentChassisInfo `json:"RedfishChassisInfo,omitempty"`
-	RedfishSystemInfo  *ComponentSystemInfo  `json:"RedfishSystemInfo,omitempty"`
-	RedfishManagerInfo *ComponentManagerInfo `json:"RedfishManagerInfo,omitempty"`
-	RedfishPDUInfo     *ComponentPDUInfo     `json:"RedfishPDUInfo,omitempty"`
+	RedfishChassisInfo *ComponentChassisInfo `json:"RedfishChassisInfo,omitempty" yaml:"RedfishChassisInfo,omitempty"`
+	RedfishSystemInfo  *ComponentSystemInfo  `json:"RedfishSystemInfo,omitempty" yaml:"RedfishSystemInfo,omitempty"`
+	RedfishManagerInfo *ComponentManagerInfo `json:"RedfishManagerInfo,omitempty" yaml:"RedfishManagerInfo,omitempty"`
+	RedfishPDUInfo     *ComponentPDUInfo     `json:"RedfishPDUInfo,omitempty" yaml:"RedfishPDUInfo,omitempty"`
 
-	RedfishOutletInfo any `json:"RedfishOutletInfo,omitempty"`
+	RedfishOutletInfo any `json:"RedfishOutletInfo,omitempty" yaml:"RedfishOutletInfo,omitempty"`
 }
 
 type ComponentEndpointStatus struct {
@@ -92,153 +92,153 @@ func (r *ComponentEndpoint) GetUID() string {
 func (r *ComponentEndpoint) IsHub() {}
 
 type ResourceID struct {
-	Oid string `json:"@odata.id"`
+	Oid string `json:"@odata.id" yaml:"@odata.id"`
 }
 type ComponentChassisInfo struct {
-	Name    string          `json:"Name,omitempty"`
-	Actions *ChassisActions `json:"Actions,omitempty"`
+	Name    string          `json:"Name,omitempty" yaml:"Name,omitempty"`
+	Actions *ChassisActions `json:"Actions,omitempty" yaml:"Actions,omitempty"`
 }
 type ComponentSystemInfo struct {
-	Name    string                 `json:"Name,omitempty"`
-	Actions *ComputerSystemActions `json:"Actions,omitempty"`
+	Name    string                 `json:"Name,omitempty" yaml:"Name,omitempty"`
+	Actions *ComputerSystemActions `json:"Actions,omitempty" yaml:"Actions,omitempty"`
 
-	EthNICInfo []*EthernetNICInfo `json:"EthernetNICInfo,omitempty"`
+	EthNICInfo []*EthernetNICInfo `json:"EthernetNICInfo,omitempty" yaml:"EthernetNICInfo,omitempty"`
 	PowerCtlInfo
-	Controls      []*Control     `json:"Controls,omitempty"`
-	SerialConsole *SerialConsole `json:"SerialConsole,omitempty"`
+	Controls      []*Control     `json:"Controls,omitempty" yaml:"Controls,omitempty"`
+	SerialConsole *SerialConsole `json:"SerialConsole,omitempty" yaml:"SerialConsole,omitempty"`
 }
 type PowerCtlInfo struct {
-	PowerURL string          `json:"PowerURL,omitempty"`
-	PowerCtl []*PowerControl `json:"PowerControl,omitempty"`
+	PowerURL string          `json:"PowerURL,omitempty" yaml:"PowerURL,omitempty"`
+	PowerCtl []*PowerControl `json:"PowerControl,omitempty" yaml:"PowerControl,omitempty"`
 }
 type PowerControl struct {
 	ResourceID
-	MemberId           string        `json:"MemberId,omitempty"`
-	Name               string        `json:"Name,omitempty"`
-	PowerCapacityWatts int           `json:"PowerCapacityWatts,omitempty"`
-	PowerConsumedWatts interface{}   `json:"PowerConsumedWatts,omitempty"`
-	OEM                *PwrCtlOEM    `json:"OEM,omitempty"`
-	RelatedItem        []*ResourceID `json:"RelatedItem,omitempty"`
+	MemberId           string        `json:"MemberId,omitempty" yaml:"MemberId,omitempty"`
+	Name               string        `json:"Name,omitempty" yaml:"Name,omitempty"`
+	PowerCapacityWatts int           `json:"PowerCapacityWatts,omitempty" yaml:"PowerCapacityWatts,omitempty"`
+	PowerConsumedWatts interface{}   `json:"PowerConsumedWatts,omitempty" yaml:"PowerConsumedWatts,omitempty"`
+	OEM                *PwrCtlOEM    `json:"OEM,omitempty" yaml:"OEM,omitempty"`
+	RelatedItem        []*ResourceID `json:"RelatedItem,omitempty" yaml:"RelatedItem,omitempty"`
 }
 type PwrCtlOEM struct {
-	Cray *PwrCtlOEMCray `json:"Cray,omitempty"`
-	HPE  *PwrCtlOEMHPE  `json:"HPE,omitempty"`
+	Cray *PwrCtlOEMCray `json:"Cray,omitempty" yaml:"Cray,omitempty"`
+	HPE  *PwrCtlOEMHPE  `json:"HPE,omitempty" yaml:"HPE,omitempty"`
 }
 type PwrCtlOEMCray struct {
-	PowerIdleWatts  int           `json:"PowerIdleWatts,omitempty"`
-	PowerLimit      *CrayPwrLimit `json:"PowerLimit,omitempty"`
-	PowerResetWatts int           `json:"PowerResetWatts,omitempty"`
+	PowerIdleWatts  int           `json:"PowerIdleWatts,omitempty" yaml:"PowerIdleWatts,omitempty"`
+	PowerLimit      *CrayPwrLimit `json:"PowerLimit,omitempty" yaml:"PowerLimit,omitempty"`
+	PowerResetWatts int           `json:"PowerResetWatts,omitempty" yaml:"PowerResetWatts,omitempty"`
 }
 type CrayPwrLimit struct {
-	Min int `json:"Min,omitempty"`
-	Max int `json:"Max,omitempty"`
+	Min int `json:"Min,omitempty" yaml:"Min,omitempty"`
+	Max int `json:"Max,omitempty" yaml:"Max,omitempty"`
 }
 type PwrCtlOEMHPE struct {
-	PowerLimit             CrayPwrLimit `json:"PowerLimit"`
-	PowerRegulationEnabled bool         `json:"PowerRegulationEnabled"`
-	Status                 string       `json:"Status"`
-	Target                 string       `json:"Target"`
+	PowerLimit             CrayPwrLimit `json:"PowerLimit" yaml:"PowerLimit"`
+	PowerRegulationEnabled bool         `json:"PowerRegulationEnabled" yaml:"PowerRegulationEnabled"`
+	Status                 string       `json:"Status" yaml:"Status"`
+	Target                 string       `json:"Target" yaml:"Target"`
 }
 type EthernetNICInfo struct {
-	RedfishId           string `json:"RedfishId"`
-	Oid                 string `json:"@odata.id"`
-	Description         string `json:"Description,omitempty"`
-	FQDN                string `json:"FQDN,omitempty"`
-	Hostname            string `json:"Hostname,omitempty"`
-	InterfaceEnabled    *bool  `json:"InterfaceEnabled,omitempty"`
-	MACAddress          string `json:"MACAddress"`
-	PermanentMACAddress string `json:"PermanentMACAddress,omitempty"`
+	RedfishId           string `json:"RedfishId" yaml:"RedfishId"`
+	Oid                 string `json:"@odata.id" yaml:"@odata.id"`
+	Description         string `json:"Description,omitempty" yaml:"Description,omitempty"`
+	FQDN                string `json:"FQDN,omitempty" yaml:"FQDN,omitempty"`
+	Hostname            string `json:"Hostname,omitempty" yaml:"Hostname,omitempty"`
+	InterfaceEnabled    *bool  `json:"InterfaceEnabled,omitempty" yaml:"InterfaceEnabled,omitempty"`
+	MACAddress          string `json:"MACAddress" yaml:"MACAddress"`
+	PermanentMACAddress string `json:"PermanentMACAddress,omitempty" yaml:"PermanentMACAddress,omitempty"`
 }
 type Control struct {
-	URL     string    `json:"URL"`
-	Control RFControl `json:"Control"`
+	URL     string    `json:"URL" yaml:"URL"`
+	Control RFControl `json:"Control" yaml:"Control"`
 }
 type ComponentManagerInfo struct {
-	Name         string             `json:"Name,omitempty"`
-	Actions      *ManagerActions    `json:"Actions,omitempty"`
-	EthNICInfo   []*EthernetNICInfo `json:"EthernetNICInfo,omitempty"`
-	CommandShell *CommandShell      `json:"CommandShell,omitempty"`
+	Name         string             `json:"Name,omitempty" yaml:"Name,omitempty"`
+	Actions      *ManagerActions    `json:"Actions,omitempty" yaml:"Actions,omitempty"`
+	EthNICInfo   []*EthernetNICInfo `json:"EthernetNICInfo,omitempty" yaml:"EthernetNICInfo,omitempty"`
+	CommandShell *CommandShell      `json:"CommandShell,omitempty" yaml:"CommandShell,omitempty"`
 }
 type ComponentPDUInfo struct {
-	Name    string                    `json:"Name,omitempty"`
-	Actions *PowerDistributionActions `json:"Actions,omitempty"`
+	Name    string                    `json:"Name,omitempty" yaml:"Name,omitempty"`
+	Actions *PowerDistributionActions `json:"Actions,omitempty" yaml:"Actions,omitempty"`
 }
 type ChassisActions struct {
-	ChassisReset ActionReset        `json:"#Chassis.Reset"`
-	OEM          *ChassisActionsOEM `json:"Oem,omitempty"`
+	ChassisReset ActionReset        `json:"#Chassis.Reset" yaml:"#Chassis.Reset"`
+	OEM          *ChassisActionsOEM `json:"Oem,omitempty" yaml:"Oem,omitempty"`
 }
 type ChassisActionsOEM struct {
-	ChassisEmergencyPower *ActionReset `json:"#Chassis.EmergencyPower,omitempty"`
+	ChassisEmergencyPower *ActionReset `json:"#Chassis.EmergencyPower,omitempty" yaml:"#Chassis.EmergencyPower,omitempty"`
 }
 type ActionReset struct {
-	AllowableValues []string `json:"ResetType@Redfish.AllowableValues"`
-	RFActionInfo    string   `json:"@Redfish.ActionInfo"`
-	Target          string   `json:"target"`
-	Title           string   `json:"title,omitempty"`
+	AllowableValues []string `json:"ResetType@Redfish.AllowableValues" yaml:"ResetType@Redfish.AllowableValues"`
+	RFActionInfo    string   `json:"@Redfish.ActionInfo" yaml:"@Redfish.ActionInfo"`
+	Target          string   `json:"target" yaml:"target"`
+	Title           string   `json:"title,omitempty" yaml:"title,omitempty"`
 }
 type ManagerActionsOEM struct {
-	ManagerFactoryReset *ActionFactoryReset `json:"#Manager.FactoryReset,omitempty"`
-	CrayProcessSchedule *ActionNamed        `json:"#CrayProcess.Schedule,omitempty"`
+	ManagerFactoryReset *ActionFactoryReset `json:"#Manager.FactoryReset,omitempty" yaml:"#Manager.FactoryReset,omitempty"`
+	CrayProcessSchedule *ActionNamed        `json:"#CrayProcess.Schedule,omitempty" yaml:"#CrayProcess.Schedule,omitempty"`
 }
 type ActionFactoryReset struct {
-	AllowableValues []string `json:"FactoryResetType@Redfish.AllowableValues"`
-	Target          string   `json:"target"`
-	Title           string   `json:"title,omitempty"`
+	AllowableValues []string `json:"FactoryResetType@Redfish.AllowableValues" yaml:"FactoryResetType@Redfish.AllowableValues"`
+	Target          string   `json:"target" yaml:"target"`
+	Title           string   `json:"title,omitempty" yaml:"title,omitempty"`
 }
 type ActionNamed struct {
-	AllowableValues []string `json:"Name@Redfish.AllowableValues"`
-	Target          string   `json:"target"`
-	Title           string   `json:"title,omitempty"`
+	AllowableValues []string `json:"Name@Redfish.AllowableValues" yaml:"Name@Redfish.AllowableValues"`
+	Target          string   `json:"target" yaml:"target"`
+	Title           string   `json:"title,omitempty" yaml:"title,omitempty"`
 }
 type ComputerSystemActions struct {
-	ComputerSystemReset ActionReset `json:"#ComputerSystem.Reset"`
+	ComputerSystemReset ActionReset `json:"#ComputerSystem.Reset" yaml:"#ComputerSystem.Reset"`
 }
 type RFControl struct {
-	ControlDelaySeconds int      `json:"ControlDelaySeconds"`
-	ControlMode         string   `json:"ControlMode"`
-	ControlType         string   `json:"ControlType"`
-	Id                  string   `json:"Id"`
-	Name                string   `json:"Name"`
-	PhysicalContext     string   `json:"PhysicalContext"`
-	SetPoint            int      `json:"SetPoint"`
-	SetPointUnits       string   `json:"SetPointUnits"`
-	SettingRangeMax     int      `json:"SettingRangeMax"`
-	SettingRangeMin     int      `json:"SettingRangeMin"`
-	Status              StatusRF `json:"Status"`
+	ControlDelaySeconds int      `json:"ControlDelaySeconds" yaml:"ControlDelaySeconds"`
+	ControlMode         string   `json:"ControlMode" yaml:"ControlMode"`
+	ControlType         string   `json:"ControlType" yaml:"ControlType"`
+	Id                  string   `json:"Id" yaml:"Id"`
+	Name                string   `json:"Name" yaml:"Name"`
+	PhysicalContext     string   `json:"PhysicalContext" yaml:"PhysicalContext"`
+	SetPoint            int      `json:"SetPoint" yaml:"SetPoint"`
+	SetPointUnits       string   `json:"SetPointUnits" yaml:"SetPointUnits"`
+	SettingRangeMax     int      `json:"SettingRangeMax" yaml:"SettingRangeMax"`
+	SettingRangeMin     int      `json:"SettingRangeMin" yaml:"SettingRangeMin"`
+	Status              StatusRF `json:"Status" yaml:"Status"`
 }
 type StatusRF struct {
-	Health       string `json:"Health"`
-	HealthRollUp string `json:"HealthRollUp,omitempty"`
-	State        string `json:"State,omitempty"`
+	Health       string `json:"Health" yaml:"Health"`
+	HealthRollUp string `json:"HealthRollUp,omitempty" yaml:"HealthRollUp,omitempty"`
+	State        string `json:"State,omitempty" yaml:"State,omitempty"`
 }
 type SerialConsole struct {
-	MaxConcurrentSessions int                    `json:"MaxConcurrentSessions"`
-	IPMI                  *SerialConsoleProtocol `json:"IPMI,omitempty"`
-	SSH                   *SerialConsoleProtocol `json:"SSH,omitempty"`
-	Telnet                *SerialConsoleProtocol `json:"Telnet,omitempty"`
-	WebSocket             *WebSocketConsole      `json:"WebSocket,omitempty"`
+	MaxConcurrentSessions int                    `json:"MaxConcurrentSessions" yaml:"MaxConcurrentSessions"`
+	IPMI                  *SerialConsoleProtocol `json:"IPMI,omitempty" yaml:"IPMI,omitempty"`
+	SSH                   *SerialConsoleProtocol `json:"SSH,omitempty" yaml:"SSH,omitempty"`
+	Telnet                *SerialConsoleProtocol `json:"Telnet,omitempty" yaml:"Telnet,omitempty"`
+	WebSocket             *WebSocketConsole      `json:"WebSocket,omitempty" yaml:"WebSocket,omitempty"`
 }
 type SerialConsoleProtocol struct {
-	ServiceEnabled        bool   `json:"ServiceEnabled"`
-	Port                  int    `json:"Port,omitempty"`
-	HotKeySequenceDisplay string `json:"HotKeySequenceDisplay,omitempty"`
-	SharedWithManagerCLI  bool   `json:"SharedWithManagerCLI,omitempty"`
-	ConsoleEntryCommand   string `json:"ConsoleEntryCommand,omitempty"`
+	ServiceEnabled        bool   `json:"ServiceEnabled" yaml:"ServiceEnabled"`
+	Port                  int    `json:"Port,omitempty" yaml:"Port,omitempty"`
+	HotKeySequenceDisplay string `json:"HotKeySequenceDisplay,omitempty" yaml:"HotKeySequenceDisplay,omitempty"`
+	SharedWithManagerCLI  bool   `json:"SharedWithManagerCLI,omitempty" yaml:"SharedWithManagerCLI,omitempty"`
+	ConsoleEntryCommand   string `json:"ConsoleEntryCommand,omitempty" yaml:"ConsoleEntryCommand,omitempty"`
 }
 type WebSocketConsole struct {
-	ServiceEnabled bool   `json:"ServiceEnabled"`
-	Interactive    bool   `json:"Interactive"`
-	ConsoleURI     string `json:"ConsoleURI"`
+	ServiceEnabled bool   `json:"ServiceEnabled" yaml:"ServiceEnabled"`
+	Interactive    bool   `json:"Interactive" yaml:"Interactive"`
+	ConsoleURI     string `json:"ConsoleURI" yaml:"ConsoleURI"`
 }
 type ManagerActions struct {
-	ManagerReset ActionReset        `json:"#Manager.Reset"`
-	OEM          *ManagerActionsOEM `json:"Oem,omitempty"`
+	ManagerReset ActionReset        `json:"#Manager.Reset" yaml:"#Manager.Reset"`
+	OEM          *ManagerActionsOEM `json:"Oem,omitempty" yaml:"Oem,omitempty"`
 }
 type CommandShell struct {
-	ServiceEnabled        bool     `json:"ServiceEnabled"`
-	MaxConcurrentSessions int      `json:"MaxConcurrentSessions"`
-	ConnectTypesSupported []string `json:"ConnectTypesSupported"`
+	ServiceEnabled        bool     `json:"ServiceEnabled" yaml:"ServiceEnabled"`
+	MaxConcurrentSessions int      `json:"MaxConcurrentSessions" yaml:"MaxConcurrentSessions"`
+	ConnectTypesSupported []string `json:"ConnectTypesSupported" yaml:"ConnectTypesSupported"`
 }
 type PowerDistributionActions struct {
-	OEM *json.RawMessage `json:"Oem,omitempty"`
+	OEM *json.RawMessage `json:"Oem,omitempty" yaml:"Oem,omitempty"`
 }
